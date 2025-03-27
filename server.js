01296e03ce8bc22f59741090495cd9205e46bdaf
Claire-bot87 <clairebrady12@hotmail.com>
2025-03-27 10:06:57 +0000

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,14 @@ import reviewController from "./controllers/reviewController.js"
 
 const app = express()
 const port = process.env.port || 3000
-app.use(cors())
+//app.use(cors())
+app.use(
+  cors({
+    origin: "https://stickypopcorn1.netlify.app",
+    methods: "GET, POST, PUT, DELETE",
+    credentials: true, 
+  })
+)
 app.use(express.json()) //# parses JSON body type, adding them to the req.body
 app.use(mongoSanitize()) //# prevent cody injections
 app.use(logger) //# logs out key information on incoming requests
